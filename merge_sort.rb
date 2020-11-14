d245de048a69b9b8d4f9d80da117054dfba959e3
NicolasArayaB <nicolasaraya@gmail.com>
2020-11-14 12:05:33 -0300

    slice arr

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,6 +1,17 @@
 def merge_sort(arr)
-  sorted_arr =
-
+  sorted_arr = []
+  if arr.size < 2
+    sorted_arr << arr
+  else
+    right = arr[0, arr.size/2]
+    left = arr[arr.size/2..-1]
+    if right[0] > left[0]
+      sorted_arr << right[0]
+    else
+      sorted_arr << left[0]
+    end
+    
+  end
 end
 
 merge_sort([2, 5, 3, 6, 7, 1])
